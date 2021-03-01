4b66ec8f2399bb4a7db1334b1587b067a9f9dbfa
chisoftltd <chisoftltd@gmail.com>
2021-03-01 19:56:17 +0100

    Removing Rows

--- PandasFixingWrongData.py
+++ PandasFixingWrongData.py
@@ -10,7 +10,15 @@ print()
 #print()
 
 for x in df.index:
-    if df.loc[x, 'Value'] > df["Value"].mean():
-        df.loc[x, 'Value'] = df["Value"].mean()
+  if df.loc[x, "Value"] > '120':
+    df.drop(x, inplace = True)
+
+#remember to include the 'inplace = True' argument to make the changes in the original DataFrame object instead of returning a copy
+
+print(df.to_string())
+
+for x in df.index:
+    if df.loc[x, "Value"] > df["Value"].mean():
+        df.loc[x, "Value"] = df["Value"].mean()
 print(df.to_string())
 print()
