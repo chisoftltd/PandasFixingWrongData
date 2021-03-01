5b67533b1a5e030729570379e67c301c9884f506
chisoftltd <chisoftltd@gmail.com>
2021-03-01 17:49:30 +0100

    Removing Rows

--- PandasFixingWrongData.py
+++ PandasFixingWrongData.py
@@ -3,13 +3,14 @@
 import pandas as pd 
 df = pd.read_csv('data.csv')
 print(df.to_string())
+
 print()
-df.loc[18, 'Transaction Date'] = '\'04/01/2021\''
-print(df.to_string())
-print()
+#df.loc[18, 'Transaction Date'] = '\'04/01/2021\''
+#print(df.to_string())
+#print()
 
 for x in df.index:
-    if df.loc[x, 'Credit Amount'] == '':
-        df.loc[x, 'Credit Amount'] = df["Debit Amount"].mean()
+    if df.loc[x, 'Value'] > df["Value"].mean():
+        df.loc[x, 'Value'] = df["Value"].mean()
 print(df.to_string())
 print()
